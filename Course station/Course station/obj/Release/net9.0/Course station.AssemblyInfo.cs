923ed81a78ec273fdf684e24965e261160bd2222
Ahmed Hassan <amh247408@gmail.com>
2024-12-08 21:13:46 +0200

    add layout.css

--- Course station/Course station/obj/Release/net9.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Release/net9.0/Course station.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfaf2d83f9a9eb0d7488da28207d83734ae7eb2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
