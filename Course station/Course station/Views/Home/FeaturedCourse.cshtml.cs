f8b2fad78ff19c6fb12babe4a9fae65382391d9f
Ahmed Hassan <amh247408@gmail.com>
2024-12-09 17:44:45 +0200

    add FeaturedCourse

---
+++ Course station/Course station/Views/Home/FeaturedCourse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Course_station.Views.Home
+{
+    public class FeaturedCourseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
