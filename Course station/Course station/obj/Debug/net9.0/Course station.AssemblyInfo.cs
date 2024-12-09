0cf901a77512f925ebb1ab2ac828201d3cd3960f
Ahmed Hassan <amh247408@gmail.com>
2024-12-09 17:41:33 +0200

    add Contact us

--- Course station/Course station/obj/Debug/net9.0/Course station.AssemblyInfo.cs
+++ Course station/Course station/obj/Debug/net9.0/Course station.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Course station")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f42fa8fe79d427e93ecc44c993cd974bd94fe61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105bb4d31aecc371547b10be9ca0be7f944d2dfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Course station")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Course station")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
